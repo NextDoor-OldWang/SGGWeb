de10edfcce6840a6e404477c9b7bc1bc836b57b1
OldWang <1033312630@qq.com>
2019-07-14 22:54:07 +0800

    3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React,{Component} from 'react';
 import {Button,message} from 'antd'
+import {BrowserRouter,HashRouter,Switch,Route} from 'react-router-dom';
+import Login from './pages/login/login';
+import Admin from './pages/admin/admin';
 export default class App extends Component{
     handleClick = () =>{
         message.success('ok');
     }
     render(){
         return(
-            <Button type='primary' onClick={this.handleClick}>学习喽</Button>
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/admin' component={Admin}/>
+                </Switch>
+            </BrowserRouter>
         )
     }
 }
